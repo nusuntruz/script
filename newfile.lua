d7b7d3a79f5d5ac4436c1ab2adb19b95d9a5c6d1
nusuntruz <75524995+nusuntruz@users.noreply.github.com>
2021-12-17 01:20:01 +0200

    Create newfile.lua

---
+++ newfile.lua
@@ -0,0 +1,10 @@
+local luatable = {
+  {name = 'SeaSide', link = 'https://raw.githubusercontent.com/nusuntruz/script/main/main.lua', filename = 'v23dwWascdr.lua'},
+	loaded = true,
+}
+
+function yes()
+	return luatable
+end
+
+return yes()
