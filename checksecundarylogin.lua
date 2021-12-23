fce7b9323ac9b4476853d4d2e87742b3ed39fefa
nusuntruz <75524995+nusuntruz@users.noreply.github.com>
2021-12-23 15:06:25 +0200

    Update checksecundarylogin.lua

--- checksecundarylogin.lua
+++ checksecundarylogin.lua
@@ -4,7 +4,7 @@ username_true['thunderzeraa'] = true
 username_true['Ruz'] = true
 username_true['GammaYTB'] = true
 username_true['Cabrons'] = true
-username_true['S2W'] == true
+username_true['S2W'] = true
 
 local function sendtrue()
     if username_true[username] == true then
