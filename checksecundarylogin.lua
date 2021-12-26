445cb751dedf372a4373b31426c449702dfef7f2
nusuntruz <75524995+nusuntruz@users.noreply.github.com>
2021-12-26 19:51:29 +0200

    Update checksecundarylogin.lua

--- checksecundarylogin.lua
+++ checksecundarylogin.lua
@@ -1,7 +1,7 @@
 local username_true = {}
 local username = globals.get_username()
 username_true['thunderzeraa'] = true
-username_true['Ruz'] = true
+username_true['Ruzule'] = true
 username_true['GammaYTB'] = true
 username_true['Cabrons'] = true
 username_true['S2W'] = true
